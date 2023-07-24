b20fc26796e898f6b040045016690dbc53cb855f
Irene Garcia-Camacho <igarcia@iri.upc.edu>
2023-07-24 22:53:34 +0200

    Measure drape coefficient
    -Computed px to cm ratio.
    -Computed drape coefficient.
    -Started verification of results.
    
    To Do:
    
    Check that the results are correct (is the measured area in cm correct to the real one?)
    
    Improve generalisation of code:
    -That works with other lightnings, fabric textures and backgrounds.
    -Ensure that it detect closed contours
    
    Save results in CSV.

--- stiffness.py
+++ stiffness.py
@@ -1,21 +1,25 @@
 import cv2
 import os
+import math
 
-data_directory = "./test"
+data_dir = "./data"
 all_files = False
-test_image = "./test/plate.jpg"
+test_image = data_dir + "/flowers10.jpg"
+cloth_dims = (17,24)
 
-total_sample_area = 0
-plate_area = 5174.5
-plate_area_cm = 153.9380
-px_cm = plate_area / plate_area_cm
+plate_image = data_dir + "/brown_plate10.jpg"
+plate_r = 10/2
+#cloth_measured_total_area = 0
 
 #########################################################
 
 def do_things(img):
     #img = cv2.imread("pink.jpg")  # Read image
     img = cv2.resize(img, (int(img.shape[1]*0.1),int(img.shape[0]*0.1)), interpolation = cv2.INTER_AREA) 
-      
+
+    # convert the image to grayscale format
+#    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
     # Setting parameter values
     t_lower = 50  # Lower Threshold
     t_upper = 150  # Upper threshold
@@ -43,38 +47,93 @@ def do_things(img):
     
     # Find largest contour to filter noise
     contours, hierarchy = cv2.findContours(dilated,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-    contour = max(contours, key = cv2.contourArea) #len
+    contour = max(contours, key = cv2.contourArea) #key=len
     
     contourImg = cv2.drawContours(img, contour, -1, (0,255,0), 3)
     cv2.imshow("Contours", contourImg)
-    
+
+#    convexHull = cv2.convexHull(contour, False)
+#    img2 = cv2.drawContours(img, [convexHull], -1, (255, 0, 0), 2)
+#    cv2.imshow("Convex", img2)
+
+
+
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 
     # Compute AREA value
     measured_area = cv2.contourArea(contour)
-    print(measured_area)
+    print("Measured contour area: ", measured_area)
+
+    return measured_area
+
+
+
+def get_px_cm_ratio(measured_plate_area):
+
+    real_plate_area_cm = math.pi*pow((plate_r),2)
+    print("Real plate area (cm): ", real_plate_area_cm)
+
+    px_cm = measured_plate_area / real_plate_area_cm
+    print(px_cm)
+
+    measured_plate_area_cm = measured_plate_area / px_cm
+    print("Measured plate area (cm): ", measured_plate_area_cm)
+
+    measured_plate_r_cm = math.sqrt(measured_plate_area_cm/math.pi)
+    print("Measured plate radius (cm): ", measured_plate_r_cm)
+
+    print("PX / CM: ", px_cm)
+
+    return px_cm
+
+def compute_drape_ratio(plate_area, cloth_total_area, cloth_measured_area):
 
     # Compute drape ratio
-    #drape = (measured_area-plate_area) / (total_sample_area-plate_area)
+    drape = (cloth_measured_area-plate_area) / (cloth_total_area-plate_area)
+
+    return drape
 
 
 
 #########################################################
 
+# Get px cm ratio using the plate as reference
+print("\033[94m Measuring px cm ratio...\033[0m")
+print(plate_image)
+plate_img = cv2.imread(plate_image)
+plate_measured_area = do_things(plate_img)
+px_cm = get_px_cm_ratio(plate_measured_area)
+plate_measured_area_cm = plate_measured_area/px_cm
+plate_real_area_cm = math.pi*pow((plate_r),2)
+
 if not (all_files):
+    print("\033[94m Measuring drape area of ", test_image, "\033[0m")
     img = cv2.imread(test_image)
-    do_things(img)
+    cloth_measured_area = do_things(img)
+    cloth_measured_area_cm = cloth_measured_area/px_cm
+    print("Cloth measured area (cm): ", cloth_measured_area_cm)
+    cloth_total_area_cm = cloth_dims[0]*cloth_dims[1]
+    print("Cloth real area (cm): ", cloth_total_area_cm)
+    drape_ratio = compute_drape_ratio(plate_measured_area_cm, cloth_total_area_cm, cloth_measured_area_cm)
+    print("\033[92m DRAPE RATIO (%): ", drape_ratio*100, " % \033[0m")
 
 if(all_files):
     print("all files")
-    for filename in sorted(os.listdir(data_directory)):
-        f = os.path.join(data_directory, filename)
+    for filename in sorted(os.listdir(data_dir)):
+        f = os.path.join(data_dir, filename)
         if os.path.isfile(f) and filename.endswith('.jpg'):
-            print(f)
+            print("\033[94m Measuring drape area of ", f, "\033[0m ")
             img = cv2.imread(f)
-            do_things(img)
+            cloth_measured_area = do_things(img)
+            cloth_measured_area_cm = cloth_measured_area/px_cm
+            print("Cloth measured area (cm): ", cloth_measured_area_cm)
+            cloth_total_area_cm = cloth_dims[0]*cloth_dims[1]
+            print("Cloth real area (cm): ", cloth_total_area_cm)
+            drape_ratio = compute_drape_ratio(plate_real_area_cm, cloth_total_area_cm, cloth_measured_area_cm)
+            print("\033[92m DRAPE RATIO (%): ", drape_ratio*100, "% \033[0m")
+
+
 
 
 #########################################################
